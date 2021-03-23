2707ab9a5ff8c9066bae6c705e9de83ae1824eca
Sam D <samidiaz3@gmail.com>
2021-03-23 18:43:09 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,7 +10,8 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    Project.add_backer(project)
+    #Project.add_backer(project)
+    project.backers << self
   end
 
 
