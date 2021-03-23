6b36a4315846ac8c0e29e8f983fc71f21af0df85
Sam D <samidiaz3@gmail.com>
2021-03-23 18:30:26 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,29 @@
+class Backer
+
+  attr_accessor :name
+  attr_reader :backed_projects
+
+
+  def initialize (name)
+    @name = name
+    @backed_projects = []
+
+  def back_project(project)
+    @backed_projects << project
+    Project.add_backer(self.name)
+  end
+
+
+
+
+
+
+  end
+
+
+
+
+
+
+
+end
---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+class Project
+
+  attr_accessor :title
+  attr_reader :backers
+
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+  end
+
+
+
+end
