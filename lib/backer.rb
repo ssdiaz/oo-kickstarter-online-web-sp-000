f1e7cfeaf149c687e61ea1aeae799b1616975412
Sam D <samidiaz3@gmail.com>
2021-03-23 18:44:38 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,23 +3,16 @@ class Backer
   attr_accessor :name
   attr_reader :backed_projects
 
-
   def initialize (name)
     @name = name
     @backed_projects = []
 
   def back_project(project)
     @backed_projects << project
-    #Project.add_backer(project)
     project.backers << self
   end
 
-
-
-
-
-
-  end
+end
 
 
 
